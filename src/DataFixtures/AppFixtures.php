ea860467809817fb491fb3c1d6af1429e870122f
keos21 <alan-gauthier@hotmail.fr>
2020-11-17 15:07:10 +0100

    Add homepage, layout and navbar

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\City;
+use App\Entity\User;
+use App\Entity\UserProfile;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,9 +12,9 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        // Generate cities
         $citiesUrl = file_get_contents("https://geo.api.gouv.fr/departements/38/communes?fields=nom&format=json&geometry=centre");
         $citiesJson = json_decode($citiesUrl);
-        
         foreach($citiesJson as $obj){
             $city = new City();
             $city->setName($obj->nom);
