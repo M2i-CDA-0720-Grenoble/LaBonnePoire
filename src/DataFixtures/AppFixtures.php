4bfe5000915d266fb60af6f8ac46d1598666fdf3
aubertcl <73121103+aubertcl@users.noreply.github.com>
2020-11-18 10:58:45 +0100

    authenticator

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,13 +3,31 @@
 namespace App\DataFixtures;
 
 use App\Entity\City;
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+
+         public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+         {
+             $this->passwordEncoder = $passwordEncoder;
+         }
     public function load(ObjectManager $manager)
     {
+        $user = new User();
+
+        $user->setEmail('admin@free.fr');
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'admin'
+            
+        ));
+        $manager->persist($user);
+
         $citiesUrl = file_get_contents("https://geo.api.gouv.fr/departements/38/communes?fields=nom&format=json&geometry=centre");
         $citiesJson = json_decode($citiesUrl);
         
